393119071bb88464ad36944b046fee060a248fae
guzun <guzun@GUZUN>
2022-05-06 15:12:48 +0300

    Proje dosyası ekle.

---
+++ StokWeb/Controllers/ServisController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StokWeb.Controllers
+{
+    public class ServisController : Controller
+    {
+        // GET: Servis
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
